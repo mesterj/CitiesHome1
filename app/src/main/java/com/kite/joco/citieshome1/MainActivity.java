ee2524186a19a315846b45f1b362bf6fa284e1d6
mesterj <mesterj@freemail.hu>
2015-09-11 10:25:07 +0200

    working local query

--- app/src/main/java/com/kite/joco/citieshome1/MainActivity.java
+++ app/src/main/java/com/kite/joco/citieshome1/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import com.kite.joco.citieshome1.pojos.Place;
 import com.kite.joco.citieshome1.pojos.PostCode;
 import com.kite.joco.citieshome1.retrofit.ZippoClient;
+import com.orm.query.Condition;
+import com.orm.query.Select;
 
 import java.util.List;
 
@@ -41,11 +43,15 @@ public class MainActivity extends Activity {
         // Ha itt nincs meg csak akkor kell net.
 
         String zipcode = etZip.getText().toString();
-  /*      List<PostCode> existingPostcodeList = PostCode.find(PostCode.class, "postcode = ?", zipcode);
+        Log.d(CITIESHOME_DB_TAG," a keresett irsz: "+ zipcode);
+        //List<PostCode> existingPostcodeList = PostCode.find(PostCode.class, "postcode = ?", zipcode);
+        List<PostCode> existingPostcodeList = Select.from(PostCode.class).where(Condition.prop("postcode").eq(zipcode)).list();
+        //Log.d(CITIESHOME_DB_TAG," ujfajta select eredménye: "+ nowhasPostcode.size());
         if (existingPostcodeList.size() >0 && existingPostcodeList != null ) {
             try {
-                Place placeid = existingPostcodeList.get(0).getPlaces().get(0);
-                tvKiir.setText(placeid.getPlace_name());
+                Long placeid = existingPostcodeList.get(0).getId();
+                Place place = Select.from(Place.class).where(Condition.prop("id").eq(placeid)).first();
+                tvKiir.setText(place.getPlace_name());
                 //tvKiir.setText(existingPostcodeList.get(0).getPlaces().get(0).getPlace_name());
                 Log.d(CITIESHOME_DB_TAG, "Megtaláltam az adatbázisban, nem kellett internetes keresés");
             }
@@ -54,7 +60,7 @@ public class MainActivity extends Activity {
             }
         }
         else {
-*/
+
             ZippoClient.get().getByPostalCode("hu", zipcode, new Callback<PostCode>() {
                 @Override
                 public void success(PostCode postCode, Response response) {
@@ -70,6 +76,12 @@ public class MainActivity extends Activity {
                     Log.d(CITIESHOME_DB_TAG, " A  PostCode lista elemeinek száma: " + postcodesize);
                     Log.d(CITIESHOME_DB_TAG, " A  Places lista elemeinek száma: " + placessize);
                     tvKiir.setText(irsz);
+                    for (PostCode pc:postCodeList){
+                        Log.d(CITIESHOME_DB_TAG, " Postcode id: " + pc.getId() + " country "+ pc.getCountry() + " irsz " + pc.getPostcode());
+                    }
+                    for (Place pl:places){
+                        Log.d(CITIESHOME_DB_TAG, " place id: " + pl.getId()+ " name: "  + pl.getPlace_name());
+                    }
                 }
 
                 @Override
@@ -77,7 +89,7 @@ public class MainActivity extends Activity {
                     tvKiir.setText(error.getLocalizedMessage());
                 }
             });
-     //   }
+        }
     }
 
     @Override
